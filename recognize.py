299d41450c2f71da078ddea033713305967e6bfc
kyonest12 <lminh9594@gmail.com>
2024-05-28 22:51:25 +0700

    fig id error

--- recognize.py
+++ recognize.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import pandas as pd
+import PIL.Image as Image
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read('trainer/trainer.yml')
@@ -24,32 +25,24 @@ for index, row in df.iterrows():
 
 print("Dictionary of id-name pairs:", list)
 
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+path = "D:/lmao/face-detect/detect-face/dataset/User1/1.1.jpg"
+img = cv2.imread(path)
 
-# Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+(height, width) = img.shape[:2]
+minW = width / 5
+minH = height / 5
+#path = input('Enter image path: ')
 
-path = input('Enter image path: ')
-img = cv2.imread(str(path))
-img = cv2.flip(img, 1) # Flip vertically
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor = 1.2,
-    minNeighbors = 5,
-    minSize = (int(minW), int(minH)),
-   )
+PIL_img = Image.open(path).convert('L')  # convert it to grayscale
+img_numpy = np.array(PIL_img, 'uint8')
+faces = faceCascade.detectMultiScale(img_numpy)
 
 for(x,y,w,h) in faces:
-
     cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
 
-    id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+    id, confidence = recognizer.predict(img_numpy[y:y + h, x:x + w])
 
     # Check if confidence is less them 100 ==> "0" is perfect match
     if (confidence < 100):
@@ -65,8 +58,6 @@ for(x,y,w,h) in faces:
     cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
 
 
-cv2.imshow('camera',img)
 
 print("\n [INFO] Exiting Program and cleanup stuff")
-cam.release()
 cv2.destroyAllWindows()
