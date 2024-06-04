b1f08edd570d6543a76eec8e149d50b6763af734
kyonest12 <lminh9594@gmail.com>
2024-06-04 14:57:11 +0700

    Change api from read path to read file

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.responses import JSONResponse
 from fastapi.responses import FileResponse
@@ -19,10 +22,29 @@ def read_excel_to_json(file_path: str) -> Dict[str, Any]:
     return json_data
 
 
-@app.post("/create")
-async def createImage(name: str = Form(...), id: str = Form(...), video_path: str = Form(...)):
+import cv2
 
-    faceDetect(video_path, id, name)
+async def createImage(name: str = Form(...), id: str = Form(...), video_file: UploadFile = Form(...)):
+
+    # Lưu video vào một tệp tạm thời để xử lý
+    video_path = f"temp/{video_file.filename}"
+    with open(video_path, "wb") as video_buffer:
+        shutil.copyfileobj(video_file.file, video_buffer)
+
+    cap = cv2.VideoCapture(video_path)
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret:
+            # Xử lý frame ở đây với hàm faceDetect
+            faceDetect(frame, id, name)
+        else:
+            break
+
+    cap.release()
+
+    # Xóa tệp tạm thời sau khi đã xử lý
+    os.remove(video_path)
+
     file_path = r"dataset/users.xlsx"
     json_data = read_excel_to_json(file_path)
 
@@ -34,6 +56,15 @@ async def train():
 
 
 @app.post("/recognize")
-async def recognize(img_path: str = Form(...)):
-    result = recog(img_path)
+async def recognize(img_file: UploadFile = Form(...)):
+    # Lưu hình ảnh vào một tệp tạm thời để xử lý
+    img_path = f"temp/{img_file.filename}"
+    with open(img_path, "wb") as img_buffer:
+        shutil.copyfileobj(img_file.file, img_buffer)
+
+    result = recog(img_path)  # Sử dụng hàm recog để nhận dạng hình ảnh
+
+    # Xóa tệp tạm thời sau khi đã xử lý
+    os.remove(img_path)
+
     return result
